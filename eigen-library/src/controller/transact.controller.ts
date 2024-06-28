8ccf48dc12856a9c1e7edc92a5c09018621ee879
permaficus <abukhalif2019@gmail.com>
2024-06-28 23:05:54 +0700

    chore(): refactor some codes

--- eigen-library/src/controller/transact.controller.ts
+++ eigen-library/src/controller/transact.controller.ts
@@ -17,3 +17,18 @@ export const newTransaction = async (req: Request, res: Response, next: NextFunc
         next(error)
     }
 }
+export const returningBooks = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const response = await Transactions.addReturnTransaction({
+            memberCode: req.body.member
+        })
+        res.status(201).json({
+            status: 'OK',
+            code: 201,
+            details: response
+        })
+    } catch (error: any) {
+        res.status(error.statusCode);
+        next(error)
+    }
+}
--- eigen-library/src/model/book.entity.ts
+++ eigen-library/src/model/book.entity.ts
@@ -6,18 +6,47 @@ export const list = async (): Promise<object | undefined> => {
         return await DB.book.findMany({
             where: {
                 stock: { not: 0 }
+            },
+            include: {
+                author: {
+                    select: {
+                        name: true
+                    }
+                }
+            },
+            omit: {
+                authorId: true
             }
-        })
+        });
     } catch (error: unknown) {
         prismaErrHandler(error);
     }
 }
 export const add = async (body: Book): Promise<Book | undefined> => {
     try {
-        return await DB.book.create({
-            data: { ...body}
-        })
+        const res = await DB.book.create({
+            data: {
+                code: body.code,
+                stock: body.stock,
+                title: body.title,
+                author: {
+                    create: {
+                        name: body.author
+                    }
+                }
+            },
+            include: {
+                author: true
+            }
+        });
+        return {
+            id: res.id,
+            code: res.code,
+            title: res.title,
+            stock: res.stock,
+            author: res.author.name
+        }
     } catch (error) {
-        
+        prismaErrHandler(error);
     }
 }
