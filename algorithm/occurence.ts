5d57334e088ae62f6276559afd0ae82ac8940549
permaficus <abukhalif2019@gmail.com>
2024-06-29 16:58:57 +0700

    chore(): update occurence algo

--- algorithm/occurence.ts
+++ algorithm/occurence.ts
@@ -1,4 +1,4 @@
-function countOccurrences(input: string[], query: string[]): number[] {
+const countingOccurences = (input: string[], query: string[]): number[] => {
     return query.map(q => input.filter(word => word === q).length);
 }
 
@@ -6,5 +6,5 @@ function countOccurrences(input: string[], query: string[]): number[] {
 const INPUT = ['xc', 'dz', 'bbb', 'dz']  
 const QUERY = ['bbb', 'ac', 'dz']  
 
-const occurrences = countOccurrences(INPUT, QUERY);
+const occurrences = countingOccurences(INPUT, QUERY);
 console.log(occurrences); // Output: [3, 2, 0]
